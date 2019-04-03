55694ceb7b2639e65b2287c80230a8b01a49789a
cmcsorley <cmcsorley@wisc.edu>
2019-04-03 09:37:58 -0500

    Module 8 Assignment Completed

--- main.js
+++ main.js
@@ -1,151 +1,87 @@
 /* Javascript by Catherine McSorley, 2019 */
-window.onload = function() {
+//begin script when window loads
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
     
-    //create the container to place elements
-    var w = 900, h = 500;
-    
-    var container = d3.select("body")
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
         .append("svg")
-        .attr("width",w)
-        .attr("height",h)
-        .attr("class","container")
-        .style("background-color","rgba(0,0,0,0.2)");
-    
-    //create the white and grey rectangle
-    var innerRect = container.append("rect")
-        .datum(400)
-        .attr("width", function(d){
-            return d*2;
-        })
-        .attr("height", function(d){
-            return d;
-        })
-        .attr("class","innerRect")
-        .attr("x",50)
-        .attr("y",50)
-        .style("fill", "#FFFFFF");
-    
-    //format populations into the array
-    var cityPop = [
-        { 
-            city: 'Glasgow',
-            population: 599650
-        },
-        {
-            city: 'Edinburgh',
-            population: 464990
-        },
-        {
-            city: 'Aberdeen',
-            population: 196670
-        },
-        {
-            city: 'Inverness',
-            population: 61235
-        }
-    ];
-    
-    //create the scale linear x variable
-    var x = d3.scaleLinear() 
-        //altered to spread acrosss rectangle
-        .range([100, 750])
-        .domain([0,3]);
-    
-    //calculate min and max pop
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
+    //create Albers equal area conic projection centered on North Carolina
+    //HELP cannot figure out how to rotate properly
+    var projection = d3.geoAlbers()
+        .center([-80.793457,35.782169])
+        .rotate([0, 0])
+        .parallels([35, 36.4])
+        .scale(2500)
+        .translate([width / 2, height / 2]);
     
-    //create the scale linear y variable
-    var y = d3.scaleLinear()
-        .range([450, 50])
-        .domain([0,700000]);
+    var path = d3.geoPath()
+        .projection(projection);
     
-    //create scaled colors for the bubbles
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];
+    promises.push(d3.csv("data/D3LabData.csv")); //load attributes from csv
+    promises.push(d3.json("data/WGS84Counties.topojson")); 
+    promises.push(d3.json("data/cb_2017_us_state_20m.topojson")); 
+    Promise.all(promises).then(callback);
     
-    //create the circles including population circle and placement, as well as spread out across the "x" range
-    var circles = container.selectAll(".circles")
-        .data(cityPop)
-        .enter()
-        .append("circle")
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            var area = d.population*0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d,i){
-            return x(i);
-        })
-        .attr("cy", function(d){
-              return y(d.population);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
-    
-    //create and labe a y axis label
-    var yAxis = d3.axisLeft(y);
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50,0)")
-        .call(yAxis);
-    
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("Scottish City Populations");
-    
-    //create labels for each bubble
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            return y(d.population) + 5;
-        })
-    
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
+    function callback(data){
+	   csvData = data[0];
+	   nc = data[1];
+	   usa = data[2];
+        
+        //HELP I struggled with finding a dataset that had the FIPS codes AND was not projected. I don't know how I'm going to connect my CSV file
+        var ncCounties = topojson.feature(nc, nc.objects.WGS84Counties).features;
+        var usStates = topojson.feature(usa, usa.objects.usaTOPO).features;
+        
+        //Example 2.5 line 3...create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+        
+        //create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
 
-    var format = d3.format(",");
-    
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15")
-        .text(function(d){
-            return "Pop. " + format(d.population);
-        });
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+        
+        //add US states to map
+        var states = map.selectAll(".states")
+            .data(usStates)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "states " + d.properties.name;
+            })
+            .attr("d", path);
+        
+
+        //add NC counties to map 
+        //.name is a placeholder for when I find data that better represent county info
+        var counties = map.selectAll(".counties")
+            .data(ncCounties)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "counties " + d.properties.name;
+            })
+            .attr("d", path); 
+    };
 };
